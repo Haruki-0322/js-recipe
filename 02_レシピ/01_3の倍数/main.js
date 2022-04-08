db3fe9cd3a24af3d855a5320f26c9380a5a0e081
Haruki-0322 <libertyfork@gmail.com>
2022-04-08 19:45:46 +0900

    [３の倍数：発展１と２」を学習しました。

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -1,11 +1,46 @@
-const genkiFunction = function(number) {
-    for (let n = 1; n <= number; n++) {
-        if (n % 3 === 0) {
-            console.log(n + "!!!!!!!")
-        } else {
-            console.log(n)
-        }
-    }
-}
+// const genkiFunction = function(number) {
+//     for (let n = 1; n <= number; n++) {
+//         if (n % 3 === 0) {
+//             console.log(n + "!!!!!!!")
+//         } else {
+//             console.log(n)
+//         }
+//     }
+// }
 
-console.log(genkiFunction(100))
+// console.log(genkiFunction(100))
+
+
+// ↓発展１-FizzBuzz
+// const fizzBuzz = function(number) {
+//     for (let n = 1; n <= number; n++) {
+//         if (n % 15 === 0) {
+//             console.log("FizzBuzz")
+//         } else if (n % 3 === 0) {
+//             console.log("Fizz")
+//         } else if (n % 5 === 0) {
+//             console.log("Buzz")
+//         } else {
+//             console.log(n)
+//         }
+//     }
+// }
+
+// console.log(fizzBuzz(100))
+
+
+// ↓発展２-３が大好き
+// const three = function(number) {
+//     for (let n = 1; n <= number; n++) {
+//         if (n % 3 === 0) {
+//             console.log(n + "!!!!!!")
+//         } else if (n % 10 === 3) {
+//             console.log(n + "!!!!!!")
+//         } else if (n / 10 === 3.1 || n / 10 === 3.2 || n / 10 === 3.3 || n / 10 === 3.4 || n / 10 ===3.5 || n / 10 === 3.6 || n / 10 === 3.7 || n / 10 ===3.8 || n / 10 === 3.8 || n / 10 === 3.9) {
+//             console.log(n + "!!!!!!")
+//         } else {
+//             console.log(n)
+//         }
+//     }
+// }
+// console.log(three(100))
