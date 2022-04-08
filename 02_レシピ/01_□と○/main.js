7b79034d6d252236bf2ff06de1947ec55e6e4405
Haruki-0322 <libertyfork@gmail.com>
2022-04-08 20:55:44 +0900

    「□と△：発展」を学習しました。

--- 02_レシピ/01_□と○/main.js
+++ 02_レシピ/01_□と○/main.js
@@ -1,7 +1,24 @@
 const figure = document.getElementById("figure")
 
-figure.onclick = function() {
-    figure.classList.toggle("rounded")
-}
+// figure.onclick = function() {
+//     figure.classList.toggle("rounded")
+// }
 // toggleはそのクラス名が要素に有れば削除し、無ければ追加する。
 
+
+// ↓発展１-いろんなイベント
+// figure.onmouseenter = () => {
+//     figure.classList.toggle("rounded")
+// }
+
+
+// ↓発展２-△も！
+figure.onclick = function() {
+    if (figure.classList.contains("square")) {
+        figure.classList.replace("square", "rounded")
+    } else if (figure.classList.contains("rounded")) {
+        figure.classList.replace("rounded", "triangle")
+    } else {
+        figure.classList.replace("triangle", "square")
+    }
+}
--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -1,5 +1,7 @@
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
+const twoTimesButton = document.getElementById("twoTimes-button")
+const minusButton = document.getElementById("minus-button")
 
 let count = 0
 
@@ -7,3 +9,14 @@ plusButton.onclick = function() {
   count += 1
   display.textContent = count
 }
+
+twoTimesButton.onclick = () => {
+  count *= 2
+  display.textContent = count
+}
+
+minusButton.onclick = () => {
+  count -= 1
+  display.textContent = count
+}
+
